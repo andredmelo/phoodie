657be0c69697d311a785a910c649524ad3f499ac
andreDmelo <andre1melo@proton.me>
2024-10-25 16:30:03 +0100

    v0.0.1 Inital commit with basic Vite React Typescript Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-neutral-100">
+        <h1 className="text-4xl font-bold text-neutral-700">
+          Vite<br/>
+          React<br/>
+          TypeScript<br/>
+          Tailwind CSS
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
